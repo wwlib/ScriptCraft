4648c3baf34e3080e374e5fdd01cc24b2178ab8c
Andrew Rapo <andrew.rapo@nttdisruption.com>
2022-01-08 11:23:27 -0500

    set default JS engine to "Nashorn"

--- src/main/java/bukkit/org/scriptcraftjs/bukkit/ScriptCraftPlugin.java
+++ src/main/java/bukkit/org/scriptcraftjs/bukkit/ScriptCraftPlugin.java
@@ -37,7 +37,7 @@ public class ScriptCraftPlugin extends JavaPlugin
             // This older fix does not work with GraalVM in R21.2.0; in this case
             // it does not return any engine ('engine' is null)
             //ScriptEngineManager scriptEngineManager = new ScriptEngineManager(null);
-            this.engine = scriptEngineManager.getEngineByName("Oracle Nashorn");
+            this.engine = scriptEngineManager.getEngineByName("Nashorn");
             if (this.engine == null) {
                 this.engine = scriptEngineManager.getEngineByName("JavaScript");
             } else {
